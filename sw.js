637a1e74111afe5b7bb0ec72a3d774d39fbb7b5e
angelmicelti <angelmicelti@users.noreply.github.com>
2026-02-18 20:44:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const urlsToCache = [
   '.',
   'index.html', // Ajusta el nombre real de tu archivo HTML
   'manifest.json',
-  'icon-192x192.png',
-  'icon-512x512.png',
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
