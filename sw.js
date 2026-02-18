ad7b0e4975c4a57faf4b4b1f2c51d8289f8742f9
angelmicelti <angelmicelti@users.noreply.github.com>
2026-02-18 20:34:24 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'eval-cache-v2';
 const urlsToCache = [
   '.',
-  './index.html', // Ajusta el nombre real de tu archivo HTML
-  './manifest.json',
+  'index.html', // Ajusta el nombre real de tu archivo HTML
+  'manifest.json',
   'icon-192x192.png',
   'icon-512x512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css',
